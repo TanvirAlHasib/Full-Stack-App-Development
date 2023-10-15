3a79519d3f8f8ed1bf028dc0ede91f0b9b520e3e
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-10-15 22:05:59 +0600

    veriable declaretion and intro done

--- InterestCalculator/app/src/main/java/com/stardust/interestcalculator/MainActivity.java
+++ InterestCalculator/app/src/main/java/com/stardust/interestcalculator/MainActivity.java
@@ -3,12 +3,27 @@ package com.stardust.interestcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edPercentage,edBuy,edSell;
+    Button bCalculate1, bCalculate2;
+    TextView tvSellPrice,tvProfit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edPercentage = findViewById(R.id.edPercentage);
+        edBuy = findViewById(R.id.edBuy);
+        edSell = findViewById(R.id.edSell);
+        bCalculate1 = findViewById(R.id.bCalculate1);
+        bCalculate2 = findViewById(R.id.bCalculate2);
+        tvSellPrice = findViewById(R.id.tvSellPrice);
+        tvProfit = findViewById(R.id.tvProfit);
     }
 }
