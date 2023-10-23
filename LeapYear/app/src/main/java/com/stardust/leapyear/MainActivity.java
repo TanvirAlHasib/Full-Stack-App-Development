cdecbda20ea52e42111166d358f9f95094e53933
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-10-23 22:32:07 +0600

    variable declaretion and intro complete

--- LeapYear/app/src/main/java/com/stardust/leapyear/MainActivity.java
+++ LeapYear/app/src/main/java/com/stardust/leapyear/MainActivity.java
@@ -3,12 +3,24 @@ package com.stardust.leapyear;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edYaer;
+    TextView tvDisplay;
+    Button bCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edYaer = findViewById(R.id.edYaer);
+        tvDisplay = findViewById(R.id.tvDisplay);
+        bCalculate = findViewById(R.id.bCalculate);
+
     }
 }
