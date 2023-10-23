a2fb937f7db9e108dd6857571018cda65da53349
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-10-23 22:06:34 +0600

    java file has completed and in xml file input type has changed to number

--- Divisible_by_5__11/app/build.gradle.kts
+++ Divisible_by_5__11/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.stardust.divisible_by_5__11"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.stardust.divisible_by_5__11"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- Divisible_by_5__11/app/src/main/java/com/stardust/divisible_by_5__11/MainActivity.java
+++ Divisible_by_5__11/app/src/main/java/com/stardust/divisible_by_5__11/MainActivity.java
@@ -3,9 +3,11 @@ package com.stardust.divisible_by_5__11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +23,29 @@ public class MainActivity extends AppCompatActivity {
         edNumber = findViewById(R.id.edNumber);
         tvDisplay = findViewById(R.id.tvDisplay);
         bCalculate = findViewById(R.id.bCalculate);
+
+        bCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((edNumber.getText().toString().length()) > 0){
+                    double number = Double.parseDouble(edNumber.getText().toString());
+                    if(number == 0){
+                        tvDisplay.setText("0 is not acceptable");
+                    } else if ((number%5==0) && (number%11==0)){
+                        tvDisplay.setText(number+" is divisible by 11 and 5");
+                    } else if (number%5 == 0) {
+                        tvDisplay.setText(number+" is only divisible by 5");
+                    } else if (number%11 == 0) {
+                        tvDisplay.setText(number+" is only divisible by 11");
+                    } else if ((number%5 != 0) && (number%11 != 0)){
+                        tvDisplay.setText(number+" is not divisible by 11 and 5");
+                    }
+                } else {
+                    edNumber.setError("Please enter number");
+                    Toast.makeText(MainActivity.this, "Please enter number", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
