e83db93026280920c17286667e52ffdc9e2676ec
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-10-23 20:30:36 +0600

    new homeWork and added 2 colors in colorls.xml

---
+++ Divisible_by_5__11/app/src/main/java/com/stardust/divisible_by_5__11/MainActivity.java
@@ -0,0 +1,14 @@
+package com.stardust.divisible_by_5__11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Divisible_by_5__11/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Divisible_by_5_&_11"
+include(":app")
