f7db921b7eaf1cc0b756e44ccbeb7714aabe3c3c
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-10-23 22:42:52 +0600

    java file completed and sdk is changed to 34

--- LeapYear/app/src/main/java/com/stardust/leapyear/MainActivity.java
+++ LeapYear/app/src/main/java/com/stardust/leapyear/MainActivity.java
@@ -3,9 +3,11 @@ package com.stardust.leapyear;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +23,23 @@ public class MainActivity extends AppCompatActivity {
         edYaer = findViewById(R.id.edYaer);
         tvDisplay = findViewById(R.id.tvDisplay);
         bCalculate = findViewById(R.id.bCalculate);
+
+        bCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((edYaer.getText().toString().length()) > 0){
+                    double year = Double.parseDouble(edYaer.getText().toString());
+                    if (((year%4 == 0) && (year%100 != 0)) || (year % 400 == 0)){
+                        tvDisplay.setText(year+" is leap year");
+                    }else {
+                        tvDisplay.setText(year+" is not leap year");
+                    }
+                }else {
+                    edYaer.setError("please enter a year");
+                    Toast.makeText(MainActivity.this, "Please enter a year", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
