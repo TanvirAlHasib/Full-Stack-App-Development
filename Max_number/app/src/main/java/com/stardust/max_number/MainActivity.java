15af30bfe265d28dd9438b668292975738545110
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-10-23 18:39:46 +0600

    variable declaration and intro complete

--- Max_number/app/src/main/java/com/stardust/max_number/MainActivity.java
+++ Max_number/app/src/main/java/com/stardust/max_number/MainActivity.java
@@ -3,12 +3,22 @@ package com.stardust.max_number;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edNumber_1,edNumber_2,edNumber_3;
+    Button bCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edNumber_1 = findViewById(R.id.edNumber_1);
+        edNumber_2 = findViewById(R.id.edNumber_2);
+        edNumber_3 = findViewById(R.id.edNumber_3);
+
     }
 }
