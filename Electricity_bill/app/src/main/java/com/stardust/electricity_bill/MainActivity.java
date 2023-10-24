369561aa42eff4629c824418a10ecc84552d5164
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-10-24 22:45:37 +0600

    in xml file input type has changed and java file has completed

--- Electricity_bill/app/src/main/java/com/stardust/electricity_bill/MainActivity.java
+++ Electricity_bill/app/src/main/java/com/stardust/electricity_bill/MainActivity.java
@@ -3,9 +3,11 @@ package com.stardust.electricity_bill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +23,36 @@ public class MainActivity extends AppCompatActivity {
         edUnits = findViewById(R.id.edUnits);
         tvDisplay = findViewById(R.id.tvDisplay);
         bCalculate = findViewById(R.id.bCalculate);
+
+        bCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((edUnits.getText().toString().length()) > 0){
+                    double units = Float.parseFloat(edUnits.getText().toString());
+                    if ((units < 250.0) && (units >= 150.0)){
+                        double bill_1 = 50.0*0.5;
+                        double unit_1 = (units-50.0)/100.0;
+                        double fraction = unit_1 - 1.0;
+                        double bill_fraction = (fraction*100.0)*1.20;
+                        double bill_2 = 100.0*0.75;
+                        double bill_sum = bill_1 + bill_fraction + bill_2;
+                        double totall_bill = (bill_sum*0.2) + bill_sum;
+                        tvDisplay.setText("Totall bill in BDT "+totall_bill);
+                    } else if (units < 150.0) {
+                        double totall_bill = (50*0.5) + ((units - 50.0)*0.75);
+                        totall_bill = (totall_bill*0.2) + totall_bill;
+                        tvDisplay.setText("Totall bill in BDT "+totall_bill);
+                    }else {
+                        double totall_bill = units*1.20;
+                        totall_bill = (totall_bill*0.2)+totall_bill;
+                        tvDisplay.setText("Totall bill in BDT "+totall_bill);
+                    }
+                }else {
+                    Toast.makeText(MainActivity.this, "Please input a number", Toast.LENGTH_SHORT).show();
+                    edUnits.setError("please input a number");
+                }
+            }
+        });
 
     }
 }
