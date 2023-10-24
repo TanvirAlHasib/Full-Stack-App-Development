994f91fee8588176b9023822f0d77e36d045d551
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-10-24 19:57:28 +0600

    added new home work electricity bill and change sdk 33 to 34

---
+++ Electricity_bill/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Electricity_bill"
+include(":app")
