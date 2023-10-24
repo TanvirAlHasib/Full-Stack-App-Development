8c9a0b727b28b6ae44fd7713311a83bcd33f8495
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-10-24 14:52:31 +0600

    java file completed and change sdk 33 to 34

--- Week_day/app/build.gradle.kts
+++ Week_day/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.stardust.week_day"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.stardust.week_day"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- Week_day/app/src/main/java/com/stardust/week_day/MainActivity.java
+++ Week_day/app/src/main/java/com/stardust/week_day/MainActivity.java
@@ -3,9 +3,11 @@ package com.stardust.week_day;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
         edWeekNumber = findViewById(R.id.edWeekNumber);
         tvDisplay = findViewById(R.id.tvDisplay);
         bCalculate = findViewById(R.id.bCalculate);
+
+        bCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((edWeekNumber.getText().toString().length()) > 0){
+                    int number = Integer.parseInt(edWeekNumber.getText().toString());
+                    if ((number != 0) && (number <= 7)){
+                        String[] day = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+                        tvDisplay.setText("The day is "+day[number-1]);
+                    }else {
+                        Toast.makeText(MainActivity.this, "Please give a number getter than 0 and less than 7", Toast.LENGTH_LONG).show();
+                    }
+                }else {
+                    edWeekNumber.setError("please enter a number");
+                    Toast.makeText(MainActivity.this, "please enter a number", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
