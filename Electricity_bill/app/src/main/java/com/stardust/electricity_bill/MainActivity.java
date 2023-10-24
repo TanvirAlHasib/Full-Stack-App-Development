754f691e314e72f7d6f56cbd7bcce48cbace6725
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-10-24 20:18:38 +0600

    variable declaretion and intro completed

--- Electricity_bill/app/src/main/java/com/stardust/electricity_bill/MainActivity.java
+++ Electricity_bill/app/src/main/java/com/stardust/electricity_bill/MainActivity.java
@@ -3,12 +3,24 @@ package com.stardust.electricity_bill;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edUnits;
+    TextView tvDisplay;
+    Button bCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edUnits = findViewById(R.id.edUnits);
+        tvDisplay = findViewById(R.id.tvDisplay);
+        bCalculate = findViewById(R.id.bCalculate);
+
     }
 }
