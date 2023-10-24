cb5acb8c29f4089c5db6ae4bbaf39fb8980c0119
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-10-24 14:37:18 +0600

    variable declaretion and intro completed

--- Week_day/app/src/main/java/com/stardust/week_day/MainActivity.java
+++ Week_day/app/src/main/java/com/stardust/week_day/MainActivity.java
@@ -3,12 +3,24 @@ package com.stardust.week_day;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edWeekNumber;
+    TextView tvDisplay;
+    Button bCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edWeekNumber = findViewById(R.id.edWeekNumber);
+        tvDisplay = findViewById(R.id.tvDisplay);
+        bCalculate = findViewById(R.id.bCalculate);
+
     }
 }
