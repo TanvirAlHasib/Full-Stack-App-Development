fd0387aef3b59cf10355c88834066aff4f202444
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-10-29 21:57:27 +0600

    added reset buuton and completed whole java file

--- TextToSpeech/app/src/main/java/com/stardust/texttospeech/MainActivity.java
+++ TextToSpeech/app/src/main/java/com/stardust/texttospeech/MainActivity.java
@@ -2,14 +2,19 @@ package com.stardust.texttospeech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edText;
-    Button bTap;
+    Button bTap, bReset;
+
+    TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,30 @@ public class MainActivity extends AppCompatActivity {
 
         edText = findViewById(R.id.edText);
         bTap = findViewById(R.id.bTap);
+        bReset = findViewById(R.id.bReset);
+
+        // text to speech initialize section
+
+        textToSpeech = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+
+            }
+        });
+
+        bTap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textToSpeech.speak(edText.getText().toString(),textToSpeech.QUEUE_FLUSH,null, null);
+            }
+        });
+
+        bReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edText.setText("");
+            }
+        });
 
     }
 }
