0b43429cd9a20f763bf41e771ad73167839893c3
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-10-29 19:28:35 +0600

    variable declaretion and intro completed

--- TextToSpeech/app/src/main/java/com/stardust/texttospeech/MainActivity.java
+++ TextToSpeech/app/src/main/java/com/stardust/texttospeech/MainActivity.java
@@ -3,12 +3,21 @@ package com.stardust.texttospeech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edText;
+    Button bTap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edText = findViewById(R.id.edText);
+        bTap = findViewById(R.id.bTap);
+
     }
 }
