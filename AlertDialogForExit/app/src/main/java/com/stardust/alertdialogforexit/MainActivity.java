ab1cf79e6d6b91ce22a0389497fb715b99a56f95
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-10-30 17:27:28 +0600

    added alert picture and completed java file

--- AlertDialogForExit/app/src/main/java/com/stardust/alertdialogforexit/MainActivity.java
+++ AlertDialogForExit/app/src/main/java/com/stardust/alertdialogforexit/MainActivity.java
@@ -1,8 +1,11 @@
 package com.stardust.alertdialogforexit;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Exit !!")
+                .setIcon(R.drawable.exit_alert)
+                .setMessage("Do you really want to exit ?")
+                .setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        Toast.makeText(MainActivity.this, "Thnaks for your concern", Toast.LENGTH_SHORT).show();
+                    }
+                }).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        MainActivity.super.onBackPressed();
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                }).show();
+
     }
 }
