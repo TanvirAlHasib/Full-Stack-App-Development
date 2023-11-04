aec833f51248187d6d9f9d8be02545f49bc3030e
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-04 19:01:52 +0600

    variable declaretion and intro done

--- Checking_internet_conection/app/src/main/java/com/stardust/checking_internet_conection/MainActivity.java
+++ Checking_internet_conection/app/src/main/java/com/stardust/checking_internet_conection/MainActivity.java
@@ -3,12 +3,18 @@ package com.stardust.checking_internet_conection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button madaraDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        madaraDetails = findViewById(R.id.madaraDetails);
+
     }
 }
