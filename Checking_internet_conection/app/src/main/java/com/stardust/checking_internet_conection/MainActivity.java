52ba5b69da7329f03f7624df8a36e6e24265f903
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-04 19:36:25 +0600

    connectivity_manager and alert_dialog applied

--- Checking_internet_conection/app/src/main/java/com/stardust/checking_internet_conection/MainActivity.java
+++ Checking_internet_conection/app/src/main/java/com/stardust/checking_internet_conection/MainActivity.java
@@ -2,12 +2,22 @@ package com.stardust.checking_internet_conection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.ContextMenu;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button madaraDetails;
+    TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +25,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         madaraDetails = findViewById(R.id.madaraDetails);
+
+        madaraDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+                if ((networkInfo != null) && (networkInfo.isAvailable())){
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setIcon(R.drawable.exit_alert)
+                            .setMessage("This page is under development and \nIt will never finished :)")
+                            .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            }).show();
+                }else {
+
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setIcon(R.drawable.exit_alert)
+                            .setMessage("You are offline")
+                            .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(MainActivity.this, "make your device online", Toast.LENGTH_SHORT).show();
+                                }
+                            }).show();
+                }
+            }
+        });
 
     }
 }
