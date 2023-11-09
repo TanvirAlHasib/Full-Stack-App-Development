4505cccc6e41327f531487befa3d0a93334264db
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-09 18:49:17 +0600

    variable intro and declaretion done for pdf loader file

--- PdfReader/app/src/main/java/com/stardust/pdfreader/PdfLoader.java
+++ PdfReader/app/src/main/java/com/stardust/pdfreader/PdfLoader.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class PdfLoader extends AppCompatActivity {
+
+    PDFView pdfView;
+    LottieAnimationView no_internetAnimation, pdf_loading;
+
+    public static String pdf_url = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf_loader);
+
+        // variable intro
+
+        pdfView = findViewById(R.id.pdfView);
+        no_internetAnimation = findViewById(R.id.no_internetAnimation);
+        pdf_loading = findViewById(R.id.pdf_loading);
+
+
     }
 }
