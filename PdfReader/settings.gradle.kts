fa911090ee891bb27bb5133163a08c0efcb532b0
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-09 13:31:14 +0600

    added maven in settings.gradle.kts

--- PdfReader/settings.gradle.kts
+++ PdfReader/settings.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URL
+
 pluginManagement {
     repositories {
         google()
@@ -10,6 +12,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { setUrl("https://jcenter.bintray.com") }
     }
 }
 
