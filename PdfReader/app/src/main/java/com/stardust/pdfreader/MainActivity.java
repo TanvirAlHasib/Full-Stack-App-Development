85bf006dba8684928290547537fa9cb2f8c7b296
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-09 19:14:53 +0600

    variable declaration and intro completed

--- PdfReader/app/src/main/java/com/stardust/pdfreader/MainActivity.java
+++ PdfReader/app/src/main/java/com/stardust/pdfreader/MainActivity.java
@@ -3,12 +3,27 @@ package com.stardust.pdfreader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout fHadith, sHadith, tHadith, fourthHadith, fifthHadith, sixthHadith, seventhHadith, eightthHadith, ninethHadith, tenthHadith;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fHadith = findViewById(R.id.fHadith);
+        sHadith = findViewById(R.id.sHadith);
+        tHadith = findViewById(R.id.tHadith);
+        fourthHadith = findViewById(R.id.fourthHadith);
+        fifthHadith = findViewById(R.id.fifthHadith);
+        sixthHadith = findViewById(R.id.sixthHadith);
+        seventhHadith = findViewById(R.id.seventhHadith);
+        eightthHadith = findViewById(R.id.eightthHadith);
+        ninethHadith = findViewById(R.id.ninethHadith);
+        tenthHadith = findViewById(R.id.tenthHadith);
+
     }
 }
