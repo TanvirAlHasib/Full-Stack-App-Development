44ec96f31b4d494f16c4c40ff833e4ae10254ef2
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-09 19:08:00 +0600

    added internet connection permission and completed pdf loader java file

--- PdfReader/app/src/main/java/com/stardust/pdfreader/PdfLoader.java
+++ PdfReader/app/src/main/java/com/stardust/pdfreader/PdfLoader.java
@@ -2,10 +2,17 @@ package com.stardust.pdfreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
 
 public class PdfLoader extends AppCompatActivity {
 
@@ -25,6 +32,28 @@ public class PdfLoader extends AppCompatActivity {
         no_internetAnimation = findViewById(R.id.no_internetAnimation);
         pdf_loading = findViewById(R.id.pdf_loading);
 
+        // internet connection checking
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if ((networkInfo != null) && (networkInfo.isAvailable())){
+
+            pdf_loading.setVisibility(View.VISIBLE);
+            pdfView.setVisibility(View.INVISIBLE);
+            pdfView.fromUri(Uri.parse(pdf_url))
+                    .onLoad(new OnLoadCompleteListener() {
+                        @Override
+                        public void loadComplete(int nbPages) {
+                            pdf_loading.setVisibility(View.GONE);
+                            pdfView.setVisibility(View.VISIBLE);
+                            Toast.makeText(PdfLoader.this, "pdf loaded successfully", Toast.LENGTH_SHORT).show();
+                        }
+                    }).load();
+
+        }else {
+            no_internetAnimation.setVisibility(View.VISIBLE);
+            Toast.makeText(PdfLoader.this, "please connect your internet and reload the page", Toast.LENGTH_LONG).show();
+        }
 
     }
 }
