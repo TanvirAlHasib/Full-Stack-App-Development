5c69b0b25d4ae450c67db1a8cbe1ae60161c867d
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-14 17:54:19 +0600

    variable declaretion and intro done

--- HomeWork_2321/app/src/main/java/com/stardust/homework_2321/MainActivity.java
+++ HomeWork_2321/app/src/main/java/com/stardust/homework_2321/MainActivity.java
@@ -3,12 +3,27 @@ package com.stardust.homework_2321;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edNumber;
+    Button button;
+    TextView tvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //variable intro
+
+        edNumber = findViewById(R.id.edNumber);
+        button = findViewById(R.id.button);
+        tvDisplay = findViewById(R.id.tvDisplay);
+
+
     }
 }
