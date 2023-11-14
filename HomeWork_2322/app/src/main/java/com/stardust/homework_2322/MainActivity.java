5da89f52be7ba1c39711e96475278f72a314de67
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-14 19:46:58 +0600

    main work done

--- HomeWork_2322/app/src/main/java/com/stardust/homework_2322/MainActivity.java
+++ HomeWork_2322/app/src/main/java/com/stardust/homework_2322/MainActivity.java
@@ -3,6 +3,7 @@ package com.stardust.homework_2322;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -23,6 +24,37 @@ public class MainActivity extends AppCompatActivity {
         edNumber = findViewById(R.id.edNumber);
         tvDisplay = findViewById(R.id.tvDisplay);
         bCalculate = findViewById(R.id.bCalculate);
+
+        //functional section
+
+        bCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edNumber.getText().toString().length() > 0){
+
+                    tvDisplay.setText("0");
+                    int number = Integer.parseInt(edNumber.getText().toString());
+                    int count = 0;
+                    long sum = 0;
+
+                    for (int i = 1; true; i++) {
+
+                        if ( i%2 == 0 ){
+                            count++;
+                            if (count < number){
+                                sum = sum + i;
+                                tvDisplay.append(" "+i);
+                            }else {
+                                tvDisplay.append("\nAnd sum is: "+sum);
+                                break;
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        });
 
 
 
