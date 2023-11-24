24b7cfd22fdbe8e8fb9f97800289cc2f4da4b0e2
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-24 15:41:14 +0600

    added app icon and added bug alert code in java file

--- HomeWork_2323/app/src/main/java/com/stardust/homework_2323/MainActivity.java
+++ HomeWork_2323/app/src/main/java/com/stardust/homework_2323/MainActivity.java
@@ -2,6 +2,8 @@ package com.stardust.homework_2323;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -36,7 +38,16 @@ public class MainActivity extends AppCompatActivity {
 
                 } else {
 
-
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setIcon(R.drawable.app_icon)
+                            .setTitle("Input Error!!")
+                            .setMessage("Please enter a number to calculate.")
+                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            }).show();
 
                 }
             }
