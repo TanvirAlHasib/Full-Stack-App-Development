a3f21aa2a411aebbf626e19b85038d35acbe776b
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-24 15:33:12 +0600

    input bug checking

--- HomeWork_2323/app/src/main/java/com/stardust/homework_2323/MainActivity.java
+++ HomeWork_2323/app/src/main/java/com/stardust/homework_2323/MainActivity.java
@@ -3,6 +3,7 @@ package com.stardust.homework_2323;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,6 +22,26 @@ public class MainActivity extends AppCompatActivity {
         edNumber = findViewById(R.id.edNumber);
         tvDisplay = findViewById(R.id.tvDisplay);
         bCalculate = findViewById(R.id.bCalculate);
+
+
+        // functional section start
+
+
+        bCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edNumber.getText().toString().length() > 0){
+
+
+
+                } else {
+
+
+
+                }
+            }
+        });
+
 
 
     }
