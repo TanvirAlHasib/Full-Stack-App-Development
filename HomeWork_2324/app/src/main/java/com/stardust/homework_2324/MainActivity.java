70e655fff064fcce5128bbf16e34c2be24211c82
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-25 18:33:28 +0600

    main work done

--- HomeWork_2324/app/src/main/java/com/stardust/homework_2324/MainActivity.java
+++ HomeWork_2324/app/src/main/java/com/stardust/homework_2324/MainActivity.java
@@ -3,6 +3,7 @@ package com.stardust.homework_2324;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,6 +23,36 @@ public class MainActivity extends AppCompatActivity {
         edNumber = findViewById(R.id.edNumber);
         tvDisplay = findViewById(R.id.tvDisplay);
         bCalculate = findViewById(R.id.bCalculate);
+
+        // functional work start
+
+        bCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edNumber.getText().toString().length() > 0){
+
+                    double number = Integer.parseInt(edNumber.getText().toString());
+                    tvDisplay.setVisibility(View.VISIBLE);
+                    tvDisplay.setText("The square natural upto"+number +"terms are :\n");
+                    double sum = 0;
+
+                    for (int i = 1; i <= number; i++) {
+
+                       tvDisplay.append((i*i)+" ");
+                       sum = sum + (i*i);
+
+                    }
+
+                    tvDisplay.append("\nThe Sum of Square Natural Number upto "+number+
+                            "terms: "+sum);
+
+                } else {
+
+
+
+                }
+            }
+        });
 
 
     }
