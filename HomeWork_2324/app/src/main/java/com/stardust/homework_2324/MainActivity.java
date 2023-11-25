efbedaa2b913586d99211cfac159d6bda6101d8a
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-25 18:36:50 +0600

    input bug has fixed

--- HomeWork_2324/app/src/main/java/com/stardust/homework_2324/MainActivity.java
+++ HomeWork_2324/app/src/main/java/com/stardust/homework_2324/MainActivity.java
@@ -2,6 +2,8 @@ package com.stardust.homework_2324;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -48,7 +50,18 @@ public class MainActivity extends AppCompatActivity {
 
                 } else {
 
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setIcon(R.drawable.app_icon)
+                            .setTitle("Input Error!!")
+                            .setMessage("Please enter a number")
+                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            }).show();
 
+                    edNumber.setError("Please enter a number, here");
 
                 }
             }
