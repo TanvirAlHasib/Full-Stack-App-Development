e407df84d890fcbb62a04873dec8161ca81831c2
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-27 22:05:29 +0600

    variable declaretion and intro done

--- SharedPrefarenceHW/app/src/main/java/com/stardust/sharedprefarencehw/MainActivity.java
+++ SharedPrefarenceHW/app/src/main/java/com/stardust/sharedprefarencehw/MainActivity.java
@@ -3,12 +3,30 @@ package com.stardust.sharedprefarencehw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edName, edFName, edMName, edEmail, edRegistration, edSSC, edHSC, edBirthDate, edNid, edPassport;
+    Button bSave, bCopyAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edName = findViewById(R.id.edName);
+        edFName = findViewById(R.id.edFName);
+        edMName = findViewById(R.id.edMName);
+        edEmail = findViewById(R.id.edEmail);
+        edRegistration = findViewById(R.id.edRegistration);
+        edSSC = findViewById(R.id.edSSC);
+        edHSC = findViewById(R.id.edHSC);
+        edBirthDate = findViewById(R.id.edBirthDate);
+        edNid = findViewById(R.id.edNid);
+        edPassport = findViewById(R.id.edPassport);
+
+
     }
 }
