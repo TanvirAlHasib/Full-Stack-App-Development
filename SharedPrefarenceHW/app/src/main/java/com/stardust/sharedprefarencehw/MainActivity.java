80619aacb1dc1564e2e3ca9613bb3f288295fa74
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-27 22:12:28 +0600

    shared preference declaretion, intro and all work done for shared preference

--- SharedPrefarenceHW/app/src/main/java/com/stardust/sharedprefarencehw/MainActivity.java
+++ SharedPrefarenceHW/app/src/main/java/com/stardust/sharedprefarencehw/MainActivity.java
@@ -2,6 +2,7 @@ package com.stardust.sharedprefarencehw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
 
     EditText edName, edFName, edMName, edEmail, edRegistration, edSSC, edHSC, edBirthDate, edNid, edPassport;
     Button bSave, bCopyAll;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
         edBirthDate = findViewById(R.id.edBirthDate);
         edNid = findViewById(R.id.edNid);
         edPassport = findViewById(R.id.edPassport);
+        bSave = findViewById(R.id.bSave);
+        bCopyAll = findViewById(R.id.bCopyAll);
+
+        sharedPreferences = getSharedPreferences(getString(R.string.app_name),MODE_PRIVATE);
+        editor = sharedPreferences.edit();
 
 
     }
