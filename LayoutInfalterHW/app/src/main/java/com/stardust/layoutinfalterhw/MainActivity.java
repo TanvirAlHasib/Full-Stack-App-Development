aa7261f4df2bdbadb15342d17b7471e43687366d
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-12-04 20:18:51 +0600

    variable declaretion and intro done

--- LayoutInfalterHW/app/src/main/java/com/stardust/layoutinfalterhw/MainActivity.java
+++ LayoutInfalterHW/app/src/main/java/com/stardust/layoutinfalterhw/MainActivity.java
@@ -3,12 +3,26 @@ package com.stardust.layoutinfalterhw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView bivagList, aboutSekhHasina;
+    ImageView homeButton;
+    LinearLayout inflaterLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bivagList = findViewById(R.id.bivagList);
+        aboutSekhHasina = findViewById(R.id.aboutSekhHasina);
+        homeButton = findViewById(R.id.homeButton);
+        inflaterLayout = findViewById(R.id.inflaterLayout);
+
+
     }
 }
