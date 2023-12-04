4ccd40c6f878bd48e29bf0c47ec08bfab616ec50
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-12-04 20:54:19 +0600

    main work done

--- LayoutInfalterHW/app/src/main/java/com/stardust/layoutinfalterhw/MainActivity.java
+++ LayoutInfalterHW/app/src/main/java/com/stardust/layoutinfalterhw/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView bivagList, aboutSekhHasina;
     ImageView homeButton;
-    LinearLayout inflaterLayout;
+    LinearLayout inflaterLayout, motherLayout;
     LayoutInflater layoutInflater;
 
     @Override
@@ -25,10 +26,43 @@ public class MainActivity extends AppCompatActivity {
         aboutSekhHasina = findViewById(R.id.aboutSekhHasina);
         homeButton = findViewById(R.id.homeButton);
         inflaterLayout = findViewById(R.id.inflaterLayout);
+        motherLayout = findViewById(R.id.motherLayout);
 
         // LayoutInflater intro
 
         layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        // main functional work
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (motherLayout.getTag().toString().contains("sekh") || motherLayout.getTag().toString().contains("bivag")){
+                    layoutInflater.inflate(R.layout.activity_main, motherLayout);
+                    motherLayout.setTag("mainActivity");
+                }
+            }
+        });
+
+        bivagList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (motherLayout.getTag().toString().contains("mainActivity") || motherLayout.getTag().toString().contains("sekh")){
+                    layoutInflater.inflate(R.layout.bivag_name, inflaterLayout);
+                    motherLayout.setTag("bivag");
+                }
+            }
+        });
+
+        aboutSekhHasina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (motherLayout.getTag().toString().contains("mainActivity") || motherLayout.getTag().toString().contains("bivag")){
+                    layoutInflater.inflate(R.layout.sekh_hasina, inflaterLayout);
+                    motherLayout.setTag("sekh");
+                }
+            }
+        });
 
 
     }
