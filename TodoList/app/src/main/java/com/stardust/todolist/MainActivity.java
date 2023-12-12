06664ab03a3a78e73ad11510ec9ebbd0f3906b5d
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-12-12 21:23:50 +0600

    implemented custom adapter and tested list view

--- TodoList/app/src/main/java/com/stardust/todolist/MainActivity.java
+++ TodoList/app/src/main/java/com/stardust/todolist/MainActivity.java
@@ -2,13 +2,62 @@ package com.stardust.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // variable intro
+
+        listView = findViewById(R.id.listView);
+
+        LocalAdapter localAdapter = new LocalAdapter();
+
+        listView.setAdapter(localAdapter);
+
+
     }
+
+    // creating customize adapter as LocalAdapter
+
+    private class LocalAdapter extends BaseAdapter {
+
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View myView = layoutInflater.inflate(R.layout.task_list, parent, false);
+
+            return myView;
+        }
+    }
+
 }
