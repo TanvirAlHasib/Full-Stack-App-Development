ec41bff9a7f47792191d40f4ebdc339bcf4f6900
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-12-13 16:39:20 +0600

    variable declaretion and intro done in task input file

--- TodoList/app/src/main/java/com/stardust/todolist/Task_input.java
+++ TodoList/app/src/main/java/com/stardust/todolist/Task_input.java
@@ -3,12 +3,19 @@ package com.stardust.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Task_input extends AppCompatActivity {
+
+    Button inputButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_input);
+
+        // variable intro
+        inputButton = findViewById(R.id.inputButton);
+
     }
 }
