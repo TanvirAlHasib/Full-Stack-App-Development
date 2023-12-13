6c45831d4b69d90d98c0812f15865d5bbb61d681
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-12-13 17:01:30 +0600

    main work done of input task activity java file

--- TodoList/app/src/main/java/com/stardust/todolist/Task_input.java
+++ TodoList/app/src/main/java/com/stardust/todolist/Task_input.java
@@ -2,12 +2,21 @@ package com.stardust.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 public class Task_input extends AppCompatActivity {
 
     Button inputButton;
+    EditText edTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,36 @@ public class Task_input extends AppCompatActivity {
 
         // variable intro
         inputButton = findViewById(R.id.inputButton);
+        edTask = findViewById(R.id.edTask);
+
+        // inputButton functional
+        inputButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // input check
+                if (edTask.getText().toString().length() > 0){
+
+                    MainActivity.hashMap = new HashMap<>();
+                    MainActivity.hashMap.put("task",edTask.getText().toString());
+                    MainActivity.arrayList.add(MainActivity.hashMap);
+                    Toast.makeText(Task_input.this, "Task has added properly", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    new AlertDialog.Builder(Task_input.this)
+                            .setIcon(R.drawable.app_icon)
+                            .setTitle("Error!!")
+                            .setMessage("Please enter your task")
+                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            }).show();
+
+                }
+            }
+        });
 
     }
 }
