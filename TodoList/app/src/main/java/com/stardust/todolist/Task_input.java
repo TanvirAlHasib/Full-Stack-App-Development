3e642fb731fba32c78d41a1a37ac6663c78b2333
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-12-13 15:57:47 +0600

    added task input activity

---
+++ TodoList/app/src/main/java/com/stardust/todolist/Task_input.java
@@ -0,0 +1,14 @@
+package com.stardust.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Task_input extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_input);
+    }
+}
