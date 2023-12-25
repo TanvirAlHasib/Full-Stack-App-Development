fee5eb0d0d564861a49c5e687a273c5cbee9d229
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-12-25 20:30:20 +0600

    variable declaretion and intro done

--- Everything/app/src/main/java/com/stardust/everything/MainActivity.java
+++ Everything/app/src/main/java/com/stardust/everything/MainActivity.java
@@ -8,14 +8,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+    GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //variable intro
+        gridView = findViewById(R.id.gridView);
+
+
     }
+
+    //adapter initialize start here
 
     public class LocalAdaptar extends BaseAdapter {
 
@@ -44,5 +54,7 @@ public class MainActivity extends AppCompatActivity {
             return myView;
         }
     }
+
+    //initialize end here
 
 }
