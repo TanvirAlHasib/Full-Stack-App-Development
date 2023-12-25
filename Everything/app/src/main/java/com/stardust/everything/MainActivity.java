258b0ddc773db55b3d7278707e2acce10ad5010e
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-12-25 20:27:33 +0600

    adapter initialize done

--- Everything/app/src/main/java/com/stardust/everything/MainActivity.java
+++ Everything/app/src/main/java/com/stardust/everything/MainActivity.java
@@ -2,7 +2,12 @@ package com.stardust.everything;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public class LocalAdaptar extends BaseAdapter {
+
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View myView = layoutInflater.inflate(R.layout.grid_view, parent);
+
+            return myView;
+        }
+    }
+
 }
